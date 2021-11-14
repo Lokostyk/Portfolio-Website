3586b000aa3aa20c6d58739d487e17fde0d764e7
Lokostyk <lokostyk12@gmail.com>
2021-11-14 23:13:53 +0100

    Added

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            <img className="backgroudImage" src="/Images/background.jpg"/>
+        </div>
+    )
+}
+
+export default Main

7eb41730e04fa7edd34431869b1c41c9c03a4c10
Lokostyk <lokostyk12@gmail.com>
2021-11-21 18:46:44 +0100

    Bugs fixed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,23 +1,46 @@
 import "./contact.scss"
 
-import React,{useState} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
 
+gsap.registerPlugin(ScrollTrigger)
 function Contact() {
+    let containerRef = useRef(null)
     const [formData,setFormData] = useState({email:"",name:"",title:"",content:""})
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            Array.from(containerRef.getElementsByTagName("input")).forEach((item,index)=>{
+                if(index === containerRef.getElementsByTagName("input").length-1) return
+                gsap.from(item,{scrollTrigger:{
+                    trigger:item,
+                    start:"top 80%"
+                },clipPath: "polygon(100% 99%, 100% 100%, 0 100%, 0 99%)",duration:1})
+            })
+            gsap.from(containerRef.getElementsByTagName("textarea"),{scrollTrigger:{
+                trigger:containerRef.getElementsByTagName("textarea"),
+                start:"top 80%"
+            },clipPath:"polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)",duration:1})
+        },100)
+    },[])
     return (
-        <section className="contactContainer" action="https://formsubmit.co/lokostyk12@gmail.com" method="POST">
+        <section className="contactContainer" ref={e=>containerRef=e}>
             <h1>Let's talk.But you first.</h1>
-            <form onSubmit={e=>e.preventDefault()}>
+            <form action="https://formsubmit.co/4c9c7d256314ffb0ad2c8324c46db738" method="POST" >
                 <input type="email" name="email" value={formData.email} placeholder="E-mail"
                      onChange={e=>setFormData({...formData,email:e.target.value})} required />
-                <input type="text" name="name" value={formData.name} placeholder="Name"
-                     onChange={e=>setFormData({...formData,name:e.target.value})} required />
-                <input type="text" value={formData.title} placeholder="Title"
+                <input type="text" name="name" value={formData.title} placeholder="Title"
                      onChange={e=>setFormData({...formData,title:e.target.value})} required />
-                <textarea name="message" value={formData.content} placeholder="Content"
+                <textarea name="message" value={formData.content} placeholder="Content" rows="7"
                     onChange={e=>setFormData({...formData,content:e.target.value})} required/>
-                <input type="submit" className="submitBtn"/>
+                <input type="submit" className="submitBtn" value="Send"/>
             </form>
+            <p>OR</p>
+            <div className="otherContacts">
+                <a href="https://www.facebook.com/marcin.ziemba.79/" target="_blank"><img src="/Images/fb.svg" /></a>
+                <a href="https://www.linkedin.com/in/marcin-ziemba-8a6171217/" target="_blank"><img src="/Images/linkedin.svg"/></a>
+            </div>
         </section>
     )
 }
